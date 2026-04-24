f0fbe5568c15e108527c8438c955b6f94ddccc55
isaiahnoelpulidosalazar <isaiahnoelpulidosalazar@gmail.com>
2026-04-24 22:34:06 +0800

    Enable trustServerCertificate in database options

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ const dbConfig = {
     database: process.env.DB_NAME,
     server: process.env.DB_SERVER,
     pool: { max: 10, min: 0, idleTimeoutMillis: 30000 },
-    options: { encrypt: true, trustServerCertificate: false }
+    options: { encrypt: true, trustServerCertificate: true }
 };
 
 async function initDB() {
